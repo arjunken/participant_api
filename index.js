174daecdd830a79c645a2605c051572914276a19
Arjun Ken <kenchappagoudra@gmail.com>
2022-11-25 21:56:01 -0800

    added vercel json file

--- index.js
+++ index.js
@@ -23,3 +23,5 @@ app.use("/participants", participants);
 //Start express server
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+export default app;
