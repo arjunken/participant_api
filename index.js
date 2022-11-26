ada8ed18acceb9dc3cf50036b1d457ecf0855ba1
Arjun Ken <kenchappagoudra@gmail.com>
2022-11-25 23:39:40 -0800

    added mongo-db server option

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ import participants from "./routes/participants.js";
 import home from "./routes/home.js";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 //Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //Database Connection
+// .connect("mongodb://localhost/participants")
 mongoose
-  .connect("mongodb://localhost/participants")
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@experiments.d1gjsmt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+  )
   .then(() => console.log("DB Connected.."))
   .catch((err) => console.log(err));
 
